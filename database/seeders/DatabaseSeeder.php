571062134b6f5e99c2f4e316fbf91aa8544c117a
Medjuil <mailuser0110@gmail.com>
2020-12-14 12:58:49 +0800

    seeder for admin is added v2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
-        // \App\Models\User::factory(10)->create();
+        //$this->call(AdminSeeder::class);
+        // \App\Models\Admin::factory(10)->create();
     }
 }
